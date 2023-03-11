bcef3d0699adaa128f716f807d1727292ba07876
Arnav Shah <arnavkshah2003@gmail.com>
2023-03-11 16:45:01 +0530

    edited HomePage

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -20,7 +20,81 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: Text("News App", style: TextStyle(color: Colors.black)),
+          leading: Builder(
+            builder: (BuildContext context) {
+              return IconButton(
+                icon: Icon(
+                  Icons.menu,
+                  color: Colors.blue.shade400,
+                ),
+                onPressed: () {
+                  // Scaffold.of(context).openDrawer();
+                },
+                // tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+              );
+            },
+          ),
+          title: Row(
+            children: [
+              Icon(Icons.location_on),
+              Text("G.T Road, Kolkata",
+                  style: TextStyle(color: Colors.black, fontSize: 12)),
+              Icon(
+                Icons.keyboard_arrow_down_rounded,
+                size: 15,
+              )
+            ],
+          ),
+          actions: [
+            Row(
+              children: [
+                Material(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  child: InkWell(
+                    onTap: () {
+                      // Navigator.pushReplacementNamed(context, '/home');
+                    },
+                    child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          border: Border.all(
+                            color: Colors.grey.shade400,
+                            width: 1,
+                          ),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        width: 70,
+                        height: 40,
+                        alignment: Alignment.center,
+                        child: Row(
+                          children: [
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Icon(Icons.stars_rounded, color: Colors.amber),
+                            Text(
+                              "599",
+                              style: TextStyle(color: Colors.black),
+                            ),
+                          ],
+                        )),
+                  ),
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width / 15,
+                ),
+                Icon(
+                  Icons.notifications,
+                  color: Colors.lightBlue.shade400,
+                  size: 30,
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width / 25,
+                ),
+              ],
+            )
+          ],
           backgroundColor: Colors.white),
       body: Column(
         children: [
